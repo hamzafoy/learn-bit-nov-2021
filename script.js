7caaa393cb14ede14070932b3c2d4cca1e47bdea
Ross Foy <hamzaoflouisville@gmail.com>
2021-09-17 16:53:00 -0400

    fixing animation and swapping the hamburger icon for menu list.

--- script.js
+++ script.js
@@ -1,11 +1,26 @@
-let icon = document.getElementsByClassName('header__nav-icon');
-let nav = document.getElementsByClassName('header__nav-menu');
-let hamburger = icon[0];
-let navBar = nav[0];
+let icon = document.querySelector('.header__nav-icon');
+let nav = document.querySelector('.header__nav-menu');
+let closeButton = document.querySelector('.nav-menu__close');
 
-hamburger.addEventListener('click', () => {
-    if(hamburger.style.display != "none") {
-        hamburger.style.display = "none";
-        navBar.style.display = "flex";
+icon.addEventListener('click', () => {
+    if(icon.style.display = "block") {
+        nav.style.animation = "animatedMenuForward 1.5s ease forwards";
+        setTimeout(() => {
+            icon.style.display = "none";
+            nav.style.display = "flex";
+        })
     }
 })
+
+closeButton.addEventListener('click', () => {
+    if(nav.style.display = "flex") {
+        nav.style.animation = "revertMenuBack 1.5s forwards";
+        setTimeout(() => {
+            icon.style.display = "block";
+            nav.style.display = "none";
+        }, 1000);
+    }
+})
+
+/* hamburger.style.display = "block";
+        navBar.style.display = "none"; */
