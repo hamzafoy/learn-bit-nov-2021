a425a125bfefdc3d1a015d4101ebf09896efc149
Ross Foy <hamzaoflouisville@gmail.com>
2021-09-17 17:06:19 -0400

    further improving animation of navbar for mobile.

--- script.js
+++ script.js
@@ -4,17 +4,19 @@ let closeButton = document.querySelector('.nav-menu__close');
 
 icon.addEventListener('click', () => {
     if(icon.style.display = "block") {
+        icon.style.animation = "revertMenuBack 1.5s ease forwards"
         nav.style.animation = "animatedMenuForward 1.5s ease forwards";
         setTimeout(() => {
             icon.style.display = "none";
             nav.style.display = "flex";
-        })
+        }, 1000)
     }
 })
 
 closeButton.addEventListener('click', () => {
     if(nav.style.display = "flex") {
         nav.style.animation = "revertMenuBack 1.5s forwards";
+        icon.style.animation = "animatedMenuForward 1.5s ease forwards";
         setTimeout(() => {
             icon.style.display = "block";
             nav.style.display = "none";
